ed83f08a4662438ae3218bdad82f591ca5616723
nly96021 <albert.faber@philips.com>
2023-05-23 01:27:40 +0200

    setting first blinky to work

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # bluepill_microzig
 test repo for experimenting with microzig on a bluepill board
 
+Create project zig init-exe
+Add depdency: git submodule add gitrepo deps/stmicro-stm32
+
+
 git submodule update --init --recursive
+set PATH=D:\arm\tools\gcc-arm-none-eabi-10.3-2021.10\bin;%PATH%
+
+arm-none-eabi-gdb -x gdbdebug ./zig-out/bin/blink.elf
+arm-none-eabi-objdump -D ./zig-out/bin/blink.elf
 
 
 Start with blink led
--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,69 @@
 const std = @import("std");
+const board = @import("board.zig");
+//const stm32 = @import("stmicro-stm32");
+//const hal = stm32.hal;
+const microzig = @import("microzig");
+const chip = microzig.chip;
+const hal = microzig.hal;
+const per = microzig.chip.peripherals;
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+pub fn printChar(c: u8) void {
+    var ch_ptr: *const u8 = &c;
+    asm volatile (
+        \\mov r0, 03 
+        \\mov r1, %[ch]
+        \\nop
+        \\bkpt #0xAB
+        :
+        : [ch] "r" (ch_ptr),
+        : "r0", "r1"
+    );
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+// http://embed.rs/articles/2016/semi-hosting-rust/
+pub fn debug_spam() void {
+    printChar('A');
+    printChar('X');
+    printChar('B');
+    printChar('C');
+}
+
+pub fn main() !void {
+
+    per.RCC.APB2ENR.modify( .{.IOPCEN = 1,});
+
+    const LED_PIN = hal.parse_pin("PC13");
+    _ = LED_PIN;
+
+    per.GPIOC.CRH.modify(.{
+        .MODE13 = 0b10,
+    });
+
+    var loop_idx: u32 = 0;
+    while (true) {
+        debug_spam();
+
+        var i: u32 = 0;
+        while (i < 300_000) : (i +%= 1) {
+            microzig.cpu.nop();
+        }
+
+        if ((loop_idx % 2) == 0) {
+            printChar('@');
+            per.GPIOC.ODR.modify(.{
+                .ODR13 = 0b1,
+            });
+        } else {
+            printChar('#');
+            per.GPIOC.ODR.modify(.{
+                .ODR13 = 0b0,
+            });
+        }
+
+        loop_idx +%= 1;
+
+        // per.GPIOC.ODR.toggle(.{
+        //     .ODR13,
+        // });
+    }
 }
